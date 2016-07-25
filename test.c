476a9eb9bf8f3413c65c003705de41762deda496
rongarret <ron@mickey>
2016-07-25 08:55:39 -0700

    Add test.c

---
+++ test.c
@@ -0,0 +1,64 @@
+
+#include "tweetnacl.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef unsigned char u8;
+typedef unsigned long u32;
+typedef unsigned long long u64;
+typedef long long i64;
+typedef const unsigned char * string;
+
+int crypto_hash_stream(u8 *out, const u8 *m, u64 n);
+
+void randombytes(u8 *s, u64 n) {
+  FILE *f = fopen("/dev/urandom", "r");
+  fread(s, n, 1, f);
+  fclose(f);
+}
+
+FILE* f;
+
+extern int crypto_hash_stream_read_block(u8* buf) {
+  return fread(buf, 1, 128, f);
+}
+
+int main(int argc, char *argv[]) {
+
+  if (argc<2) {
+    printf("Usage: %s [file]\n", argv[0]);
+    exit(-1);
+  }
+
+  f = fopen(argv[1], "r");
+  fseeko(f, 0, SEEK_END);
+  size_t n = ftello(f);
+  rewind(f);
+  string m = malloc(n);
+  u64 mlen = fread((void*)m, 1, n, f);
+  if (n!=mlen) {
+    fprintf(stderr, "Input length mismatch: %lld %ld\n", mlen, n);
+    exit(-1);
+  }
+  printf("Hashing %lld bytes\n\n", mlen);
+
+  u8 h[crypto_hash_BYTES];
+  crypto_hash(h, m, mlen);
+
+  printf("crypto_hash:\n");
+  for (int i=0; i<crypto_hash_BYTES; i++) printf("%02x", h[i]);
+  printf("\n\n");
+
+  rewind(f);
+  crypto_hash_stream(h, m, mlen);
+  fclose(f);
+  printf("crypto_hash_stream:\n");
+  for (int i=0; i<crypto_hash_BYTES; i++) printf("%02x", h[i]);
+  printf("\n\n");
+
+  printf("shasum -a 512:\n");
+  char buf[1024];
+  sprintf(buf, "shasum -a 512 %s", argv[1]);
+  system(buf);
+}
